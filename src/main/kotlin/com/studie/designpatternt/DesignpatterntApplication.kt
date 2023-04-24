b0d7307c428e8ac4ad8a0e01bcb8d8075f2322bd
Tiago De Oliveira Santos <tqi_tsantos@uolinc.com>
2023-04-24 10:31:32 -0300

    first commit

---
+++ src/main/kotlin/com/studie/designpatternt/DesignpatterntApplication.kt
@@ -0,0 +1,11 @@
+package com.studie.designpatternt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesignpatterntApplication
+
+fun main(args: Array<String>) {
+	runApplication<DesignpatterntApplication>(*args)
+}
